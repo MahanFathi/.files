ee6e7976b8e1824c46795292ed6885b9056697dc
Mahan Fathi <mahan0fathi@gmail.com>
2020-09-18 14:57:11 +0430

    Branch from vaio

--- .config/i3/scripts/monitorscripts/monitorlaptop.sh
+++ .config/i3/scripts/monitorscripts/monitorlaptop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output VIRTUAL1 --off
+xrandr --output DP-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-0 --off --output DP-1 --off --output DP-2 --off
